060c7f7e1e154af29b38075061e3415c49469227
neelam <neelam.ingeniousmindslab@gmail.com>
2021-08-15 21:49:09 +0530

    new page

---
+++ lib/p.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class MyProj extends StatelessWidget {
+  const MyProj({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("hii"),
+    );
+  }
+}
